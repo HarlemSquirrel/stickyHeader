2570a5b61e0c127b9b2cc23b3318811325e5b39a
Kevin McCormack <kem2028@med.cornell.edu>
2018-02-28 13:49:25 -0500

    Update stickyheader.jquery.js
    - Move table header position and size evaluation to function
    - Reevaluate on page resize
    - Reevalute on scroll if the body height has changed

--- stickyheader.jquery.js
+++ stickyheader.jquery.js
@@ -1,15 +1,31 @@
+// https://github.com/HarlemSquirrel/stickyHeader/tree/dynamic-page-size
+
 jQuery(document).ready(function ($) {
 	var tables = $('table.stickyHeader');
-	tables.each(function(i){
+
+	tables.each(function (i) {
 		var table = tables[i];
+		var bodyHeight, cutoffTop, cutoffBottom, tableHeight, tableWidth;
+
+		function evaluateHeaderPositionAndSize() {
+			bodyHeight = $('body').height()
+			tableHeight = $(table).height();
+			tableWidth = $(table).width() +
+									 Number($(table).css('padding-left').replace(/px/ig,"")) +
+									 Number($(table).css('padding-right').replace(/px/ig,"")) +
+									 Number($(table).css('border-left-width').replace(/px/ig,"")) +
+									 Number($(table).css('border-right-width').replace(/px/ig,""));
+			cutoffTop = $(table).offset().top;
+			cutoffBottom = tableHeight + cutoffTop - headerCellHeight;
+
+			stickyHeader.css('width', tableWidth);
+		}
+
 		var tableClone = $(table).clone(true).empty().removeClass('stickyHeader');
 		var theadClone = $(table).find('thead').clone(true);
 		var stickyHeader =  $('<div></div>').addClass('stickyHeader hide').attr('aria-hidden', 'true');
 		stickyHeader.append(tableClone).find('table').append(theadClone);
 		$(table).after(stickyHeader);
-
-		var tableHeight = $(table).height();
-		var tableWidth = $(table).width() + Number($(table).css('padding-left').replace(/px/ig,"")) + Number($(table).css('padding-right').replace(/px/ig,"")) + Number($(table).css('border-left-width').replace(/px/ig,"")) + Number($(table).css('border-right-width').replace(/px/ig,""));
 
 		var headerCells = $(table).find('thead th');
 		var headerCellHeight = $(headerCells[0]).height();
@@ -19,8 +35,9 @@ jQuery(document).ready(function ($) {
 			no_fixed_support = true;
 		}
 
+		evaluateHeaderPositionAndSize()
+
 		var stickyHeaderCells = stickyHeader.find('th');
-		stickyHeader.css('width', tableWidth);
 		var cellWidths = [];
 		for (var i = 0, l = headerCells.length; i < l; i++) {
 			cellWidths[i] = $(headerCells[i]).width();
@@ -29,17 +46,16 @@ jQuery(document).ready(function ($) {
 			$(stickyHeaderCells[i]).css('width', cellWidths[i]);
 		}
 
-		var cutoffTop = $(table).offset().top;
-		var cutoffBottom = tableHeight + cutoffTop - headerCellHeight;
+    window.onresize = function () {
+    	evaluateHeaderPositionAndSize()
+    }
 
 		$(window).scroll(function() {
-			// Reevaluate the table height and position in case it has changed
-			tableHeight = $(table).height();
-			cutoffTop = $(table).offset().top;
-			cutoffBottom = tableHeight + cutoffTop - headerCellHeight;
+			if (bodyHeight !== $('body').height()) {
+				evaluateHeaderPositionAndSize()
+			}
 
 		  var currentPosition = $(window).scrollTop();
-
 			if (currentPosition > cutoffTop && currentPosition < cutoffBottom) {
 				stickyHeader.removeClass('hide');
 				if (no_fixed_support) {
