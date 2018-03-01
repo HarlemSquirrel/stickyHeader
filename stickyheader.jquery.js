76e999baca75165b3b861c1174413821a5960f13
Kevin McCormack <kem2028@med.cornell.edu>
2018-03-01 14:05:52 -0500

    Update stickyheader.jquery.js
    - Fix th widths
    - Use spaces instead of tabs for indentation

--- stickyheader.jquery.js
+++ stickyheader.jquery.js
@@ -1,70 +1,67 @@
 // https://github.com/HarlemSquirrel/stickyHeader/tree/dynamic-page-size
 
 jQuery(document).ready(function ($) {
-	var tables = $('table.stickyHeader');
+  var tables = $('table.stickyHeader');
 
-	tables.each(function (i) {
-		var table = tables[i];
-		var bodyHeight, cutoffTop, cutoffBottom, tableHeight, tableWidth;
+  tables.each(function (i) {
+    var table = tables[i];
+    var bodyHeight, cutoffTop, cutoffBottom, tableHeight, tableWidth;
 
-		function evaluateHeaderPositionAndSize() {
-			bodyHeight = $('body').height()
-			tableHeight = $(table).height();
-			tableWidth = $(table).width() +
-									 Number($(table).css('padding-left').replace(/px/ig,"")) +
-									 Number($(table).css('padding-right').replace(/px/ig,"")) +
-									 Number($(table).css('border-left-width').replace(/px/ig,"")) +
-									 Number($(table).css('border-right-width').replace(/px/ig,""));
-			cutoffTop = $(table).offset().top;
-			cutoffBottom = tableHeight + cutoffTop - headerCellHeight;
+    var tableClone = $(table).clone(true).empty().removeClass('stickyHeader');
+    var theadClone = $(table).find('thead').clone(true);
+    var stickyHeader =  $('<div></div>').addClass('stickyHeader hide').attr('aria-hidden', 'true');
+    stickyHeader.append(tableClone).find('table').append(theadClone);
+    $(table).after(stickyHeader);
 
-			stickyHeader.css('width', tableWidth);
-		}
+    var headerCells = $(table).find('thead th');
+    var headerCellHeight = $(headerCells[0]).height();
 
-		var tableClone = $(table).clone(true).empty().removeClass('stickyHeader');
-		var theadClone = $(table).find('thead').clone(true);
-		var stickyHeader =  $('<div></div>').addClass('stickyHeader hide').attr('aria-hidden', 'true');
-		stickyHeader.append(tableClone).find('table').append(theadClone);
-		$(table).after(stickyHeader);
-
-		var headerCells = $(table).find('thead th');
-		var headerCellHeight = $(headerCells[0]).height();
-
-		var no_fixed_support = false;
-		if (stickyHeader.css('position') == "absolute") {
-			no_fixed_support = true;
-		}
-
-		evaluateHeaderPositionAndSize()
-
-		var stickyHeaderCells = stickyHeader.find('th');
-		var cellWidths = [];
-		for (var i = 0, l = headerCells.length; i < l; i++) {
-			cellWidths[i] = $(headerCells[i]).width();
-		}
-		for (var i = 0, l = headerCells.length; i < l; i++) {
-			$(stickyHeaderCells[i]).css('width', cellWidths[i]);
-		}
-
-    window.onresize = function () {
-    	evaluateHeaderPositionAndSize()
+    var no_fixed_support = false;
+    if (stickyHeader.css('position') == "absolute") {
+      no_fixed_support = true;
     }
 
-		$(window).scroll(function() {
-			if (bodyHeight !== $('body').height()) {
-				evaluateHeaderPositionAndSize()
-			}
+    var stickyHeaderCells = stickyHeader.find('th');
 
-		  var currentPosition = $(window).scrollTop();
-			if (currentPosition > cutoffTop && currentPosition < cutoffBottom) {
-				stickyHeader.removeClass('hide');
-				if (no_fixed_support) {
-					stickyHeader.css('top', currentPosition + 'px');
-				}
-			}
-			else {
-				stickyHeader.addClass('hide');
-			}
-		});
-	});
+    function evaluateHeaderPositionAndSize() {
+      bodyHeight = $('body').height()
+      tableHeight = $(table).height();
+      tableWidth = $(table).width() +
+                   Number($(table).css('padding-left').replace(/px/ig,"")) +
+                   Number($(table).css('padding-right').replace(/px/ig,"")) +
+                   Number($(table).css('border-left-width').replace(/px/ig,"")) +
+                   Number($(table).css('border-right-width').replace(/px/ig,""));
+      cutoffTop = $(table).offset().top;
+      cutoffBottom = tableHeight + cutoffTop - headerCellHeight;
+
+      for (var i = 0, l = headerCells.length; i < l; i++) {
+        stickyHeaderCells[i].style.width = headerCells[i].scrollWidth + 'px'
+      }
+
+      stickyHeader.css('width', tableWidth);
+    }
+
+    evaluateHeaderPositionAndSize()
+
+    window.onresize = function () {
+      evaluateHeaderPositionAndSize()
+    }
+
+    $(window).scroll(function() {
+      if (bodyHeight !== $('body').height()) {
+        evaluateHeaderPositionAndSize()
+      }
+
+      var currentPosition = $(window).scrollTop();
+      if (currentPosition > cutoffTop && currentPosition < cutoffBottom) {
+        stickyHeader.removeClass('hide');
+        if (no_fixed_support) {
+          stickyHeader.css('top', currentPosition + 'px');
+        }
+      }
+      else {
+        stickyHeader.addClass('hide');
+      }
+    });
+  });
 });
